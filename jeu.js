f4b02679a13724065d9bee26c773c922763836a2
Efe ERKEN <efeerkenn@hotmail.com>
2023-07-27 12:05:23 +0100

    Add first set of modifications

--- jeu.js
+++ jeu.js
@@ -1,12 +1,22 @@
 /*
-NOM :
-Prénom :
+NOM : ERKEN
+Prénom : Efe
 
-NOM :
-Prénom :
+NOM : GÖKAL
+Prénom : İlber
 
 */
 
 function alea(min, max){ // [0;15[
-  return Math.floor((Math.random()*(max-min))+min)
+    return Math.floor((Math.random()*(max-min))+min)
 }
+
+$(document).ready(
+    function() {
+        for (let i = 0; i < 16; i++) {
+            var pathFull = "<div class='tuile'><img src='img/0" + i + ".jpg'/><p>" + i + "</p></div>";
+            // alert(pathFull);
+            $("#puzzlearea").append(pathFull);
+        }
+    }
+);
