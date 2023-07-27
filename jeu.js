aa7b696b36b731f39365a22e12a60890a03f97a7
Efe ERKEN <efeerkenn@hotmail.com>
2023-07-27 12:06:17 +0100

    Add second set of modifications

--- jeu.js
+++ jeu.js
@@ -14,9 +14,48 @@ function alea(min, max){ // [0;15[
 $(document).ready(
     function() {
         for (let i = 0; i < 16; i++) {
-            var pathFull = "<div class='tuile'><img src='img/0" + i + ".jpg'/><p>" + i + "</p></div>";
+            if (i == 15) {
+                var pathFull = "<div class='tuile' id='t0" + i + "'><p>" + i + "</p></div>";
+            } else {
+                var pathFull = "<div class='tuile' id='t0" + i + "'><img src='img/0" + i + ".jpg'/><p>" + i + "</p></div>";
+            }
             // alert(pathFull);
             $("#puzzlearea").append(pathFull);
+
+            var idTag = "#t0" + i;
+            // alert(idTag);
+            $(idTag).css("order", String(i + 1));
+            // alert($(idTag).css("order"));
         }
+        // $("#t015").css("order", "1");
+        // $("#t00").css("order", "16");
+    }
+);
+
+// var tuileOrd = $("#t015").css("order");
+// var tuileNum = $("#t015 p").text();
+// var tuileOrd2 = getComputedStyle(document.getElementById("t015")).getPropertyValue("order");
+// alert(String(tuileOrd));
+// alert(String(tuileNum));
+
+$("#shuffle").click(
+    function shuffle() {
+        for (let i = 0; i < 50; i++) {
+            var n = alea(0,16);
+            var m = alea(0,16);
+            if (n == m){
+                i--;
+            }
+            var idTag1 = "#t0" + n;
+            var idTag2 = "#t0" + m;
+            $(idTag1).css("order", String(m + 1));
+            $(idTag2).css("order", String(n + 1));
+        }
+    }
+);
+
+$(".tuile").click(
+    function check_and_swap() {
+
     }
 );
