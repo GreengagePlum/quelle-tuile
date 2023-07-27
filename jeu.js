72f4bb45382329187804de8471751501d8c7e622
Efe ERKEN <efeerkenn@hotmail.com>
2023-07-27 12:09:26 +0100

    Add fifth set of modifications

--- jeu.js
+++ jeu.js
@@ -20,22 +20,19 @@ $(document).ready(
                 var pathFull = "<div class='tuile' id='t0" + i + "'><img src='img/0" + i + ".jpg'/><p>" + i + "</p></div>";
             }
             $("#puzzlearea").append(pathFull);
-
-            var idTag = "#t0" + i;
-            $(idTag).css("order", String(i + 1));
+            $("#t0" + i).css("order", String(i + 1));
         }
-
 
         $("#shuffle").click( // fonction pour mélanger les tuiles
             function shuffle() {
-                for (let i = 0; i < 50; i++) {
-                    var n = alea(0,16);
-                    var m = alea(0,16);
-                    if (n == m) {
+                for (let i = 0; i < 50; i++) { // choix de 50 pour suiffisamment mélanger les tuiles
+                    var idNumAlea1 = alea(0,16);
+                    var idNumAlea2 = alea(0,16);
+                    if (idNumAlea1 == idNumAlea2) {
                         i--;
                     }
-                    var idTag1 = "#t0" + n;
-                    var idTag2 = "#t0" + m;
+                    var idTag1 = "#t0" + idNumAlea1;
+                    var idTag2 = "#t0" + idNumAlea2;
                     var ord1 = $(idTag1).css("order");
                     var ord2 = $(idTag2).css("order");
                     $(idTag1).css("order", ord2);
@@ -49,32 +46,42 @@ $(document).ready(
                 var ordClicked = $(this).css("order");
                 var idClicked = "#" + $(this).attr("id");
                 $(".tuile").each(
-                    function() {
+                    function() { // on itere sur les 16 tuiles pour trouver celles qui ont la valeur css "order" rechercé
                         var ordMatch = $(this).css("order");
                         var idMatch = "#" + $(this).attr("id");
-                        if (![1, 5, 9, 13].includes(parseInt(ordClicked))) {
+                        var isMatch = 0;
+                        if (![1, 5, 9, 13].includes(parseInt(ordClicked))) { // trouver la tuile gauche et verifier si elle est vide
                             if (ordMatch == parseInt(ordClicked) - 1 && idMatch == "#t015") {
                                 $(idClicked).css("order", ordMatch);
                                 $(idMatch).css("order", ordClicked);
+                                isMatch = 1;
                             }
                         }
-                        if (![4, 8, 12, 16].includes(parseInt(ordClicked))) {
+                        if (!isMatch && ![4, 8, 12, 16].includes(parseInt(ordClicked))) { // trouver la tuile droite et verifier si elle est vide
                             if (ordMatch == parseInt(ordClicked) + 1 && idMatch == "#t015") {
                                 $(idClicked).css("order", ordMatch);
                                 $(idMatch).css("order", ordClicked);
+                                isMatch = 1;
                             }
                         }
-                        if (ordClicked <= 12) {
-                            if (ordMatch == parseInt(ordClicked) + 4 && idMatch == "#t015") {
+                        if (!isMatch && ordClicked <= 12) {
+                            if (ordMatch == parseInt(ordClicked) + 4 && idMatch == "#t015") { // trouver la tuile en haut et verifier si elle est vide
+
                                 $(idClicked).css("order", ordMatch);
                                 $(idMatch).css("order", ordClicked);
+                                isMatch = 1;
                             }
                         }
-                        if (ordClicked >= 5) {
-                            if (ordMatch == parseInt(ordClicked) - 4 && idMatch == "#t015") {
+                        if (!isMatch && ordClicked >= 5) {
+                            if (ordMatch == parseInt(ordClicked) - 4 && idMatch == "#t015") { // trouver la tuile en bas et verifier si elle est vide
+
                                 $(idClicked).css("order", ordMatch);
                                 $(idMatch).css("order", ordClicked);
+                                isMatch = 1;
                             }
+                        }
+                        if (isMatch) { // optimisation pour ne pas contiuner à iterer et tester après avoir changé de place
+                            return;
                         }
                     }
                 );
@@ -84,10 +91,11 @@ $(document).ready(
                     function() {
                         var ord = $(this).css("order");
                         var id = $(this).attr("id");
-                        if (parseInt(id.slice(1)) + 1 == parseInt(ord)) {
+                        if (parseInt(id.slice(1)) + 1 == parseInt(ord)) { // on verifie si les tuiles sont en ordre (css "ordre" = id + 1)
                             countCorrect++;
-                            if (countCorrect == 16) {
+                            if (countCorrect == 16) { // si tout est correcte on termine le jeu
                                 $("#output").append("Bien joué, vous avez gagné !");
+                                $(".tuile").off("click");
                             }
                         }
                     }
@@ -96,100 +104,3 @@ $(document).ready(
         );
     }
 );
-
-
-/* (Version 1)
-
-        $(".tuile").click(
-            function check_and_swap() {
-                // alert("The order value of clicked element is (" + $(this).css("order") + ")");
-                var ordClicked = $(this).css("order");
-                var idClicked = "#" + $(this).attr("id");
-                if (![1, 5, 9, 13].includes(parseInt(ordClicked))) {
-                    $(".tuile").each(
-                        function() {
-                            var ordMatch = $(this).css("order");
-                            var idMatch = "#" + $(this).attr("id");
-                            if (ordMatch == ordClicked - 1 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                    );
-                }
-                if (![4, 8, 12, 16].includes(parseInt(ordClicked))) {
-                    $(".tuile").each(
-                        function() {
-                            var ordMatch = $(this).css("order");
-                            var idMatch = "#" + $(this).attr("id");
-                            if (ordMatch == ordClicked + 1 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                    );
-                }
-                if (ordClicked <= 12) {
-                    $(".tuile").each(
-                        function() {
-                            var ordMatch = $(this).css("order");
-                            var idMatch = "#" + $(this).attr("id");
-                            if (ordMatch == ordClicked + 4 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                    );
-                }
-                if (ordClicked >= 5) {
-                    $(".tuile").each(
-                        function() {
-                            var ordMatch = $(this).css("order");
-                            var idMatch = "#" + $(this).attr("id");
-                            if (ordMatch == ordClicked - 4 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                    );
-                }
-            }
-        );
-
-*/
-
-/* (Version 2)
-
-                $(".tuile").each(
-                    function check_and_swap() {
-                        var ordMatch = $(this).css("order");
-                        var idMatch = "#" + $(this).attr("id");
-                        // alert(ordClicked <= 12);
-                        if (![1, 5, 9, 13].includes(parseInt(ordClicked))) {
-                            if (ordMatch == parseInt(ordClicked) - 1 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                        if (![4, 8, 12, 16].includes(parseInt(ordClicked))) {
-                            if (ordMatch == parseInt(ordClicked) + 1 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                        if (ordClicked <= 12) {
-                            if (ordMatch == parseInt(ordClicked) + 4 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                        if (ordClicked >= 5) {
-                            if (ordMatch == parseInt(ordClicked) - 4 && idMatch == "#t015") {
-                                $(idClicked).css("order", ordMatch);
-                                $(idMatch).css("order", ordClicked);
-                            }
-                        }
-                    }
-                );
-
-*/
